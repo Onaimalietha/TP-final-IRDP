04aaec4cb5c933aa18a42f496da71b1c2c0707ef
Kou Kinoshita <kouichi.but.the.ichi.is.silent@gmail.com>
2024-12-26 16:58:52 -0500

    Created requirements file

---
+++ imports.py
@@ -0,0 +1,11 @@
+# Listing imports from jupyter notebooks to make requirements.txt
+
+# USAGE; 
+#   pip install pipreqs
+#   pipreqs /path/to/project --force
+
+import numpy as np
+import pandas as pd
+from keras.datasets import cifar10
+import matplotlib.pyplot as plt
+import cv2
